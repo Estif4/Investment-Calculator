5d384e868180dd8e28915652d3a10ff6ee0759d6
estif4 <estifk6@gmail.com>
2024-11-02 21:52:20 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Replace 4000 with your preferred port
-  },
-})
+  base: "Estif4/Investment-Calculator",
+});
